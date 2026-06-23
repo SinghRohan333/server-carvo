080772d0737c923ed6f34af2551d4a234aceb4d5
SinghRohan333 <rohansingh12345602042@gmail.com>
2026-06-23 08:01:07 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -7,12 +9,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Welcome To Carvo Server",
-  });
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("carvo-db");
+
+    app.get("/", (req, res) => {
+      res.status(200).json({
+        success: true,
+        message: "Welcome To Carvo Server",
+      });
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Server is running on port: ", port);
